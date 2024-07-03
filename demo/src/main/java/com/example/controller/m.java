40ab803fd2476357dd4d6ad76a857a128919178d
lucky <1094568708@qq.com>
2024-07-03 10:36:31 +0800

    add my

---
+++ demo/src/main/java/com/example/controller/m.java
@@ -0,0 +1,11 @@
+package com.example.controller;
+
+/**
+ * @ClassName m
+ * @Description TODO
+ * @Author lukcy
+ * @Date 2024/7/3 10:35
+ * @Version 1.0
+ */
+public class m {
+}
---
+++ demo/src/main/java/com/example/controller/my.java
@@ -0,0 +1,11 @@
+package com.example.controller;
+
+/**
+ * @ClassName my
+ * @Description TODO
+ * @Author lukcy
+ * @Date 2024/7/3 10:34
+ * @Version 1.0
+ */
+public class my {
+}
